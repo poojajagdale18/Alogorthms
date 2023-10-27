318af6c359d680cb86d24b50192bc8f8905ad5a8
poojajagdale18 <poojajagdale18@gmail.com>
2023-10-27 11:08:19 +0530

    Update ArrayTwoSumProblem.java

--- ArrayTwoSumProblem.java
+++ ArrayTwoSumProblem.java
@@ -15,16 +15,30 @@ class TwoSumArrayProblem {
       System.out.println(findTwoSum({1,3,4,9,2}, 11));
   }
 
+  //private static int[] findTwoSum(int nums[], int target) {
+  //  for (int p1=0; p1< nums.length()-1; p1 ++) {
+  //    int numToFind = target - nums[p1];
+  //    for (int p2 = p1 +1; nums.length(); p2++) {
+  //      if (numToFind == nums[p2]) {
+  //        return new int[p1, p2];
+  //      }
+  //    }
+  //    return null;
+  //  }
+
   private static int[] findTwoSum(int nums[], int target) {
-    for (int p1=0; p1< nums.length()-1; p1 ++) {
-      int numToFind = target - nums[p1];
-      for (int p2 = p1 +1; nums.length(); p2++) {
-        if (numToFind == nums[p2]) {
-          return new int[p1, p2];
+    HashMap hashmap = new HashMap<int,int>();
+      for (int p1=0; p1<nums.length(); p1++) {
+        int numsTofind = target - nums[p1];
+        if(hashMap.has(numsToFind)) {
+          return new int {hashMap.getValue(numsToFind), p1};
         }
+        hashmap.put(nums[p1], p1);
+        
       }
-      return null;
-    }
+
+    return null;
   }
+  
   
 }
